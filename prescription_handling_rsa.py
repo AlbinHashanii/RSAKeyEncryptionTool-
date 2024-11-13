c257275280f2bef0e8f13d146b0fa36588df007a
ArjanaaTernava <arijanaternava1@gmail.com>
2024-11-13 17:16:46 +0100

    Add functionality to sign and encrypt the prescription using RSA keys

--- prescription_handling_rsa.py
+++ prescription_handling_rsa.py
@@ -1,6 +1,7 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import serialization, hashes
+
 
 #RSA keys for the doctor and the pharmacy
 def generate_rsa_keypair():
@@ -44,5 +45,41 @@ def display_keys():
         format=serialization.PublicFormat.SubjectPublicKeyInfo
     ).decode())
 
+#Doctor writes and signs the prescription, then encrypts it for the pharmacy
+def create_and_encrypt_prescription(prescription, doctor_private_key, pharmacy_public_key):
+    signature = doctor_private_key.sign(
+        prescription.encode("utf-8"),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    encrypted_prescription = pharmacy_public_key.encrypt(
+        prescription.encode("utf-8"),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    return encrypted_prescription, signature
+
+#Input
+prescription_text = input("\nEnter the prescription text: ")
 
 display_keys()
+
+encrypted_prescription, signature = create_and_encrypt_prescription(
+    prescription_text, doctor_private_key, pharmacy_public_key
+)
+
+with open("encrypted_prescription.bin", "wb") as enc_file:
+    enc_file.write(encrypted_prescription)
+print("\nPrescription encrypted and signed by the doctor.")
+print("Encrypted prescription saved to 'encrypted_prescription.bin'.")
+
+
+
